e1eb6cd27bfee56e4cee6fc4231a13beac0c79ed
dabasrianto <dabasrianto@gmail.com>
2025-12-11 03:27:45 +0000

    pwa

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "jam-hijriah-v1";
+const urlsToCache = ["/", "/index.html", "/clock-icon.svg", "/vite.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
